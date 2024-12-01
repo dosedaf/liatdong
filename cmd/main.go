d59df4e813299f0af42d0d7e1613009b6153600e
dosedaf <mash1o1o1o1@gmail.com>
2024-12-01 08:10:41 +0700

    refactor: adjust main to align with file instance

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
-	"log"
-	"net/http"
-
 	"liatdong/internal/config"
 )
 
 func main() {
 	viperConfig := config.NewViperConfig()
-	_ = config.NewLogger(viperConfig)
-	_ = config.NewPgxConnection(viperConfig)
+	app := config.NewFiberApp(viperConfig)
+	log := config.NewLogger(viperConfig)
+	pool, err := config.NewPgxConnection(viperConfig)
+	if err != nil {
+		// gimana ya?
+	}
+
+	config.Bootstrap(&config.ConfigInstance{
+		Pool: pool,
+		App:  app,
+		Log:  log,
+		// Validate: validate,
+		Config: viperConfig,
+	})
 
 	log.Println("Starting server on :8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
+	if err := app.Listen(":8080"); err != nil {
+		log.Fatalf(err.Error())
 	}
 }
